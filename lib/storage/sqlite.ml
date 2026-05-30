bae9ace0184fa82dcc3862d4ba92cecc9bd85d0c
tom <tom@tksfz.org>
2026-05-29 22:11:45 -0700

    add storage for schema_sources

--- lib/storage/sqlite.ml
+++ lib/storage/sqlite.ml
@@ -51,7 +51,21 @@ let init_tables (module C : Caqti_lwt.CONNECTION) =
           C.exec create_idx_name () >>= function
           | Error e -> Lwt.return (Db.or_error (Error e))
           | Ok () ->
-              C.exec create_entity_data () >|= Db.or_error
+              C.exec create_entity_data () >>= function
+              | Error e -> Lwt.return (Db.or_error (Error e))
+              | Ok () ->
+                  let create_schema_sources =
+                    (Caqti_type.unit ->. Caqti_type.unit)
+                      "CREATE TABLE IF NOT EXISTS schema_sources ( \
+                       id TEXT PRIMARY KEY, \
+                       created_at REAL NOT NULL, \
+                       filename TEXT NOT NULL, \
+                       file_hash TEXT NOT NULL UNIQUE, \
+                       lang TEXT NOT NULL, \
+                       source TEXT NOT NULL \
+                       )"
+                  in
+                  C.exec create_schema_sources () >|= Db.or_error
 
 let create_database path =
   Db.connect path >>= function
@@ -162,3 +176,47 @@ let get_entity_data (module C : Caqti_lwt.CONNECTION) id =
             let data = Yojson.Safe.from_string data_str in
             Ok (Some Entdb_data.Entity_data.{ id; entity_definition_id = def_id; data })
         | Error e, _ | _, Error e -> Error (Trait.SerializationError e))
+
+let schema_source_row_type =
+  Caqti_type.(t2 string (t2 float (t2 string (t2 string (t2 string string)))))
+
+let row_to_schema_source (id_str, (created_at, (filename, (file_hash, (lang_str, source))))) =
+  match (Entdb_data.Type_id.of_string id_str, Entdb_data.Schema_source.lang_of_string lang_str) with
+  | Error e, _ | _, Error e -> Error (Trait.SerializationError e)
+  | Ok id, Ok lang -> Ok Entdb_data.Schema_source.{ id; created_at; filename; file_hash; lang; source }
+
+let insert_schema_source (module C : Caqti_lwt.CONNECTION) (s : Entdb_data.Schema_source.t) =
+  let open Caqti_request.Infix in
+  let query =
+    (schema_source_row_type ->. Caqti_type.unit)
+      "INSERT INTO schema_sources (id, created_at, filename, file_hash, lang, source) VALUES (?, ?, ?, ?, ?, ?)"
+  in
+  C.exec query (Entdb_data.Type_id.to_string s.id, (s.created_at, (s.filename, (s.file_hash, (Entdb_data.Schema_source.lang_to_string s.lang, s.source))))) >|= Db.or_error
+
+let get_all_schema_sources (module C : Caqti_lwt.CONNECTION) =
+  let open Caqti_request.Infix in
+  let query =
+    (Caqti_type.unit ->* schema_source_row_type)
+      "SELECT id, created_at, filename, file_hash, lang, source FROM schema_sources ORDER BY created_at ASC"
+  in
+  C.fold query (fun row acc ->
+      match acc with
+      | Error e -> Error e
+      | Ok lst -> (
+          match row_to_schema_source row with
+          | Ok s -> Ok (s :: lst)
+          | Error e -> Error e)) () (Ok []) >|= function
+  | Ok (Ok lst) -> Ok (List.rev lst)
+  | Ok (Error e) -> Error e
+  | Error e -> Db.or_error (Error e)
+
+let get_schema_source_by_hash (module C : Caqti_lwt.CONNECTION) hash =
+  let open Caqti_request.Infix in
+  let query =
+    (Caqti_type.string ->? schema_source_row_type)
+      "SELECT id, created_at, filename, file_hash, lang, source FROM schema_sources WHERE file_hash = ?"
+  in
+  C.find_opt query hash >|= Db.or_error >>= function
+  | Error e -> Lwt.return (Error e)
+  | Ok None -> Lwt.return (Ok None)
+  | Ok (Some row) -> Lwt.return (match row_to_schema_source row with Ok x -> Ok (Some x) | Error e -> Error e)
